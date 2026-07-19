432baad2268326d26a7117785500292ff47f1875
Bruno Banguela Miguel <bruno.banguela@unitel.co.ao>
2026-07-19 12:55:15 +0100

    creating new directories

---
+++ src/main/java/ao/co/berp/BerpApiApplication.java
@@ -0,0 +1,13 @@
+package ao.co.berp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerpApiApplication.class, args);
+	}
+
+}
